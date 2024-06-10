b66e3213ee1035240d1f7b7b501a35aa26a4d8f6
Ben Carruthers <ben_carruthers@hotmail.co.uk>
2024-06-11 00:04:56 +0100

    headlines.pl accepts input to open articles

--- headlines.pl
+++ headlines.pl
@@ -7,25 +7,42 @@ use strict;
 use warnings;
 use Getopt::Std;
 use HTTP::Tiny;
+# use Browser::Open qw( open_browser );
 use Data::Dumper;
 
 
 #+ GLOBAL VARIABLES +#
 
 # scalars
-my $url = "https://www.theguardian.com/uk";
-my $response = HTTP::Tiny->new->get( $url );
+my $url = "https://www.theguardian.com";
+my $response = HTTP::Tiny->new->get( $url . "/uk" );
 my $counter = 1;
 
 die "No response from Guardian\n" unless $response->{success};
 die "Couldn't find section" unless $response->{content} =~ m{<section.+data-link-name="most-viewed"[^>]+>(.+?)</section};
 
-my @items = $1 =~ m{<li[^>]+>(.+?)</li>}g;
+my @raw_items = $1 =~ m{<li[^>]+>(.+?)</li>}g;
+my @hashes = ();
 
-foreach( @items ) {
-  $_ =~ m{<span>(.+)</span>} && print ($counter++, " - ", $1, "\n" );
+foreach( @raw_items ) {
+  next unless $_ =~ m{href=["']([^"']+).*<span>(.+)</span>};
+  my %hash = (
+    url => $url . $1,
+    headline => $2,
+    num => $counter++,
+  );
+  print($hash{num}, " - ", $hash{headline}, "\n");
+  push(@hashes, \%hash);
 }
 
-# print Dumper(\@items);
+print ("\nEnter a number to read an article... ");
+my $input = <>;
+chomp($input);
+exit 0 unless $input =~ m{^([1-9]|10)\s*$};
+foreach ( @hashes ) {
+  next unless $_->{num} == $input;
+  `open $_->{url}`;
+  print ("Opening ", $_->{url}, "\n");
+}
 
 exit 0;
